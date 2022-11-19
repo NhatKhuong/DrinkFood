6dd1dc5483f0894f9ee0dfa01bc2c16b9bfc0284
DangNhatKhuong <nhatkhuong2001@gmail.com>
2022-11-19 17:07:25 +0700

    discount voucher process

--- views/Voucher.js
+++ views/Voucher.js
@@ -6,8 +6,11 @@ import Checkbox from 'expo-checkbox';
 import { useState } from 'react';
 import { Radio, NativeBaseProvider, Center } from "native-base";
 
-export default function Voucher() {
-  const [isChecked, setChecked] = useState(false);
+export default function Voucher({navigation}) {
+  const [discount, setDiscount] = useState(0)
+  const [discount2, setDiscount2] = useState(0)
+  console.log(discount);
+  console.log(discount2);
   return (
       <NativeBaseProvider>
         <View style={{backgroundColor:"#000",flex:1,paddingLeft:10, paddingRight:10}}>
@@ -28,7 +31,9 @@ export default function Voucher() {
             </View>
           </View>
           <View style={{flex:0.4}}>
-            <Radio.Group>
+            <Radio.Group onChange={nextValue=>{
+              setDiscount(nextValue)
+            }}>
             <Text style={{color:"#fff", fontSize:18, fontWeight:"bold"}}>Ưu đãi vận chuyển</Text>
             <Text style={{color:"#d4ed9b", fontSize:16}}>Có thể chọn 1 Voucher</Text>
             <View style={{display:"flex", flexDirection:"row", backgroundColor:"#fff",borderRadius:20, marginTop:10}}>
@@ -46,7 +51,7 @@ export default function Voucher() {
                   onValueChange={setChecked}
                   color={true ? '#4630EB' : undefined}
                 /> */}
-                <Radio value="one" my={1} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
+                <Radio  value="1" my={1} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
                  
                
               </View>
@@ -62,7 +67,7 @@ export default function Voucher() {
               </View>
               <View style={{flex:0.2, alignItems:"center",justifyContent:"center"}}>
 
-              <Radio value="two" my={1} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
+              <Radio value="2" my={1} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
 
               </View>
             </View>
@@ -73,7 +78,9 @@ export default function Voucher() {
             <Text style={{color:"#d4ed9b", fontSize:16}}>Có thể chọn 1 Voucher</Text>
             
             <ScrollView style={{height:400}}>
-            <Radio.Group>
+            <Radio.Group onChange={nextValue=>{
+              setDiscount2(nextValue)
+            }}>
 
               <View style={{display:"flex", flexDirection:"row", backgroundColor:"#fff",borderRadius:20, marginTop:10}}>
                 <View style={{flex:0.35}}>
@@ -84,7 +91,7 @@ export default function Voucher() {
                   <Text style={{color:"#fff", color:"#2cc167", fontWeight:"bold"}}>Sắp hết hạn: Còn 1 ngày</Text>
                 </View>
                 <View style={{flex:0.2, alignItems:"center",justifyContent:"center"}}>
-                <Radio value="one" my={2} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
+                <Radio value="15" my={2} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
               </View>
               </View>
 
@@ -97,14 +104,14 @@ export default function Voucher() {
                   <Text style={{color:"#fff", color:"#2cc167", fontWeight:"bold"}}>Sắp hết hạn: Còn 1 ngày</Text>
                 </View>
               <View style={{flex:0.2, alignItems:"center",justifyContent:"center"}}>
-               <Radio value="two" my={2} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
+               <Radio value="10" my={2} style={{width:20, height:20}} ><Text style={{opacity:0}}>a</Text></Radio>
               </View>
               </View>
               </Radio.Group>
             </ScrollView>
           </View>
           
-          <TouchableOpacity style={{backgroundColor:"#2cc167", width:"100%",height:50, borderRadius:5, borderColor:"#fff",alignSelf:"center",alignItems:"center", justifyContent:"center"}}>
+          <TouchableOpacity style={{backgroundColor:"#2cc167", width:"100%",height:50, borderRadius:5, borderColor:"#fff",alignSelf:"center",alignItems:"center", justifyContent:"center"}} onPress={()=>navigation.navigate('OderView',{discount:discount,discount2:discount2})}>
               <Text style={{color:"#fff", fontSize:16, fontWeight:'bold'}}>Áp dụng</Text>
           </TouchableOpacity>
         </View>
